c8860a6570b702c208a055f2eaeae2c282c40d8c
Juan Ramirez <ep.juan@gmail.com>
2018-01-01 19:45:36 -0500

    Corrección Carga de Imagenes

--- FilesFolders/Form1.Designer.cs
+++ FilesFolders/Form1.Designer.cs
@@ -30,14 +30,22 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.carpetasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.rIPSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAC = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblUS = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.lblStatusUS = new System.Windows.Forms.Label();
             this.prgBarUS = new System.Windows.Forms.ProgressBar();
+            this.btnUS = new System.Windows.Forms.Button();
             this.lblStatusAC = new System.Windows.Forms.Label();
             this.prgBarAC = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lblTotalUS = new System.Windows.Forms.Label();
             this.lblTotalAT = new System.Windows.Forms.Label();
@@ -58,14 +66,6 @@
             this.btnRuta = new System.Windows.Forms.Button();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.btnUS = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.lblUS = new System.Windows.Forms.Label();
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.lblAC = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -91,6 +91,13 @@
             this.archivoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.A)));
             this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
             this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
             // carpetasToolStripMenuItem
             // 
@@ -134,6 +141,57 @@
             this.panel1.Size = new System.Drawing.Size(698, 257);
             this.panel1.TabIndex = 1;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(14, 197);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "DOC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "AP";
+            // 
+            // lblAC
+            // 
+            this.lblAC.AutoSize = true;
+            this.lblAC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAC.Location = new System.Drawing.Point(14, 131);
+            this.lblAC.Name = "lblAC";
+            this.lblAC.Size = new System.Drawing.Size(23, 13);
+            this.lblAC.TabIndex = 19;
+            this.lblAC.Text = "AC";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblTitle.Location = new System.Drawing.Point(12, 62);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(142, 21);
+            this.lblTitle.TabIndex = 18;
+            this.lblTitle.Text = "Corregir Archivos";
+            // 
+            // lblUS
+            // 
+            this.lblUS.AutoSize = true;
+            this.lblUS.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUS.Location = new System.Drawing.Point(14, 102);
+            this.lblUS.Name = "lblUS";
+            this.lblUS.Size = new System.Drawing.Size(24, 13);
+            this.lblUS.TabIndex = 17;
+            this.lblUS.Text = "US";
+            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(96, 187);
@@ -160,6 +218,16 @@
             this.prgBarUS.Size = new System.Drawing.Size(331, 23);
             this.prgBarUS.TabIndex = 14;
             // 
+            // btnUS
+            // 
+            this.btnUS.Image = global::FilesFolders.Properties.Resources.icons8_Play32_32;
+            this.btnUS.Location = new System.Drawing.Point(51, 97);
+            this.btnUS.Name = "btnUS";
+            this.btnUS.Size = new System.Drawing.Size(36, 23);
+            this.btnUS.TabIndex = 13;
+            this.btnUS.UseVisualStyleBackColor = true;
+            this.btnUS.Click += new System.EventHandler(this.btnUS_Click);
+            // 
             // lblStatusAC
             // 
             this.lblStatusAC.AutoSize = true;
@@ -175,6 +243,16 @@
             this.prgBarAC.Name = "prgBarAC";
             this.prgBarAC.Size = new System.Drawing.Size(331, 23);
             this.prgBarAC.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::FilesFolders.Properties.Resources.Shutdown_32px;
+            this.button1.Location = new System.Drawing.Point(640, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 40);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
             // groupBox1
             // 
@@ -306,6 +384,7 @@
             // 
             // btnAC
             // 
+            this.btnAC.Image = global::FilesFolders.Properties.Resources.icons8_Play32_32;
             this.btnAC.Location = new System.Drawing.Point(51, 126);
             this.btnAC.Name = "btnAC";
             this.btnAC.Size = new System.Drawing.Size(36, 23);
@@ -322,6 +401,7 @@
             // 
             // btnAP
             // 
+            this.btnAP.Image = global::FilesFolders.Properties.Resources.icons8_Play32_32;
             this.btnAP.Location = new System.Drawing.Point(51, 155);
             this.btnAP.Name = "btnAP";
             this.btnAP.Size = new System.Drawing.Size(36, 23);
@@ -346,82 +426,6 @@
             this.btnRuta.Text = "Ruta";
             this.btnRuta.UseVisualStyleBackColor = true;
             this.btnRuta.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnUS
-            // 
-            this.btnUS.Location = new System.Drawing.Point(51, 97);
-            this.btnUS.Name = "btnUS";
-            this.btnUS.Size = new System.Drawing.Size(36, 23);
-            this.btnUS.TabIndex = 13;
-            this.btnUS.UseVisualStyleBackColor = true;
-            this.btnUS.Click += new System.EventHandler(this.btnUS_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(640, 214);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(45, 40);
-            this.button1.TabIndex = 10;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
-            // 
-            // salirToolStripMenuItem
-            // 
-            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
-            this.salirToolStripMenuItem.Text = "Salir";
-            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
-            // 
-            // lblUS
-            // 
-            this.lblUS.AutoSize = true;
-            this.lblUS.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUS.Location = new System.Drawing.Point(14, 102);
-            this.lblUS.Name = "lblUS";
-            this.lblUS.Size = new System.Drawing.Size(24, 13);
-            this.lblUS.TabIndex = 17;
-            this.lblUS.Text = "US";
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.ForeColor = System.Drawing.Color.DarkGreen;
-            this.lblTitle.Location = new System.Drawing.Point(12, 62);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(142, 21);
-            this.lblTitle.TabIndex = 18;
-            this.lblTitle.Text = "Corregir Archivos";
-            // 
-            // lblAC
-            // 
-            this.lblAC.AutoSize = true;
-            this.lblAC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAC.Location = new System.Drawing.Point(14, 131);
-            this.lblAC.Name = "lblAC";
-            this.lblAC.Size = new System.Drawing.Size(23, 13);
-            this.lblAC.TabIndex = 19;
-            this.lblAC.Text = "AC";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 160);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(23, 13);
-            this.label2.TabIndex = 20;
-            this.label2.Text = "AP";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(14, 197);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(33, 13);
-            this.label8.TabIndex = 21;
-            this.label8.Text = "DOC";
             // 
             // Form1
             // 
