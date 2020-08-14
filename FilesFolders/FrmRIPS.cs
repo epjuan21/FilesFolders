8fa0d9eca81e84bc34ab9b3d8036c085a3acd796
Juan Fernando Ramirez <ep.juan@gmail.com>
2020-08-14 09:09:46 -0500

    Correccion Errores

--- FilesFolders/FrmRIPS.cs
+++ FilesFolders/FrmRIPS.cs
@@ -2615,7 +2615,7 @@ namespace FilesFolders
                                     // S33000 HORA PROFESIONAL MEDICO TRASLADO PACIENTES
                                     if (split[6] == "S33000")
                                     {
-                                        split[6] = "602T01   ";
+                                        split[6] = "602T01";
                                         line = String.Join(",", split);
                                         contadorErrores++;
                                     }
